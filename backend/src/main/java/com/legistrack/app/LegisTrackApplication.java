a02a92f47194c099969a47f8a9ca72ca01dc0a3c
Vn0m <juanpapolo123@gmail.com>
2026-01-18 23:39:46 -0500

    format pom.xml

--- backend/src/main/java/com/legistrack/app/LegisTrackApplication.java
+++ backend/src/main/java/com/legistrack/app/LegisTrackApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 
-// turn on again to test embeddings
 @SpringBootApplication(exclude = {
     org.springframework.ai.vectorstore.pgvector.autoconfigure.PgVectorStoreAutoConfiguration.class
 })
